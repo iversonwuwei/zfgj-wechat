c6ffaa24795ba9484e6298260caac63ecae812f9
walden <wuwei@housecenter.cn>
2017-05-31 16:05:30 +0800

    update

--- dlfc-zfw-wechat-search/src/main/java/com/dlfc/services/search/controller/UserController.java
+++ dlfc-zfw-wechat-search/src/main/java/com/dlfc/services/search/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.dlfc.services.search.controller;
 
-import com.dlfc.services.search.repository.HouseRService;
+import com.alibaba.fastjson.JSONObject;
 import com.dlfc.services.search.repository.UserInfoRService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +15,8 @@ public class UserController {
     private UserInfoRService userInfoRService;
 
     @RequestMapping(value = "/all", method = RequestMethod.GET)
-    public String findAll(){
-        String yes = userInfoRService.findUserById("3c007a5512804d5183d737b4c9e5b26a");
+    public JSONObject findAll(){
+        JSONObject yes = userInfoRService.findUserById("3c007a5512804d5183d737b4c9e5b26a");
         return yes;
     }
 }
--- dlfc-zfw-wechat-search/src/main/java/com/dlfc/services/search/repository/UserInfoRService.java
+++ dlfc-zfw-wechat-search/src/main/java/com/dlfc/services/search/repository/UserInfoRService.java
@@ -1,6 +1,7 @@
 package com.dlfc.services.search.repository;
 
 
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,5 +11,5 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface UserInfoRService {
 
     @RequestMapping(value = "/wc/datas/usrUser/findById", method = RequestMethod.GET)
-    String findUserById(@RequestParam(name = "id") String id);
+    JSONObject findUserById(@RequestParam(name = "id") String id);
 }
