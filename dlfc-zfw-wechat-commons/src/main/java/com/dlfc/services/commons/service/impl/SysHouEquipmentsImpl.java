94c68c9046d731f9074dfbd9f5290fb3cbfa42d8
walden <wuwei@housecenter.cn>
2017-06-03 16:04:08 +0800

    add config

--- dlfc-zfw-wechat-commons/src/main/java/com/dlfc/services/commons/service/impl/SysHouEquipmentsImpl.java
+++ dlfc-zfw-wechat-commons/src/main/java/com/dlfc/services/commons/service/impl/SysHouEquipmentsImpl.java
@@ -5,12 +5,14 @@ import com.dlfc.services.commons.repository.RepositoryService;
 import com.dlfc.services.commons.service.DataService;
 import com.housecenter.dlfc.commons.bases.convertor.base.IConvertor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
  * Created by K on 2017/6/2.
  */
+@Service
 public class SysHouEquipmentsImpl implements DataService<SysHouEquipmentsContrast> {
 
     private String result;
