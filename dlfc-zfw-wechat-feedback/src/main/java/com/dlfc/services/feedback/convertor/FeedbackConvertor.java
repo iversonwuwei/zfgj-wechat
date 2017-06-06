f09430ba16e68dd4e7e85d7625bf2a4a0b7484e5
walden <wuwei@housecenter.cn>
2017-06-06 15:30:56 +0800

    order

--- dlfc-zfw-wechat-feedback/src/main/java/com/dlfc/services/feedback/convertor/FeedbackConvertor.java
+++ dlfc-zfw-wechat-feedback/src/main/java/com/dlfc/services/feedback/convertor/FeedbackConvertor.java
@@ -14,7 +14,7 @@ public class FeedbackConvertor extends AbstractConvertor<UsrFeedback, FeedbackDT
     public UsrFeedback toModel(FeedbackDTO feedbackDTO) {
         UsrFeedback usrFeedback = new UsrFeedback();
         usrFeedback.setContent(feedbackDTO.getFeedback());
-
+        usrFeedback.setId("3c007a5512804d5183d737b4c9e5b26a");
         return usrFeedback;
     }
 
