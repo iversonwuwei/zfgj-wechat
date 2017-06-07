c0727bb3625378a2cd47911a6c5451f0b13df8a2
walden <wuwei@housecenter.cn>
2017-06-07 12:45:12 +0800

    update properties

--- dlfc-zfw-wechat-feedback/src/main/java/com/dlfc/services/feedback/controller/FeedbackController.java
+++ dlfc-zfw-wechat-feedback/src/main/java/com/dlfc/services/feedback/controller/FeedbackController.java
@@ -3,10 +3,12 @@ package com.dlfc.services.feedback.controller;
 
 import com.dlfc.services.feedback.convertor.FeedbackConvertor;
 import com.dlfc.services.feedback.dto.FeedbackDTO;
+import com.dlfc.services.feedback.dto.UserDTO;
 import com.dlfc.services.feedback.entity.CmsGuestbook;
 import com.dlfc.services.feedback.entity.UsrFeedback;
 import com.dlfc.services.feedback.repository.UserInfoRService;
 import com.dlfc.services.feedback.service.FeedbackService;
+import com.housecenter.dlfc.commons.bases.convertor.base.IConvertor;
 import com.housecenter.dlfc.commons.bases.dto.ResultDTO;
 import com.housecenter.dlfc.framework.ca.api.PrincipalService;
 import com.housecenter.dlfc.framework.common.web.AjaxResult;
@@ -25,12 +27,15 @@ public class FeedbackController {
     private PrincipalService principalService;
     @Autowired
     private UserInfoRService userInfoRService;
+    @Autowired
+    private IConvertor convertor;
 
     @RequestMapping(method = RequestMethod.POST)
     public ResultDTO<Void> feedback(@RequestBody FeedbackDTO feedbackDTO, @RequestHeader String token){
         AjaxResult user = principalService.principal(token);
         String uid = userInfoRService.findUserByUser(user.getData().toString());
-        feedbackDTO.setUid(uid);
+        UserDTO userDTO = (UserDTO) convertor.convert2Object(uid, UserDTO.class);
+        feedbackDTO.setUid(userDTO.getId());
         UsrFeedback usrFeedback = feedbackConvertor.toModel(feedbackDTO);
         String id = (String) feedbackFeedbackService.feedback(usrFeedback);
         if (id == null){
