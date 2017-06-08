e4e04a7cb75a50ca036c68fd917c1181d004221b
walden <wuwei@housecenter.cn>
2017-06-08 18:17:12 +0800

    registry

--- dlfc-zfw-wechat-collect/src/main/java/com/dlfc/services/collect/entity/UsrHouCollection.java
+++ dlfc-zfw-wechat-collect/src/main/java/com/dlfc/services/collect/entity/UsrHouCollection.java
@@ -1,8 +1,6 @@
 package com.dlfc.services.collect.entity;
 
 
-import jdk.nashorn.internal.objects.annotations.Getter;
-import jdk.nashorn.internal.objects.annotations.Setter;
 
 
 import java.util.Date;
