a2a55a724982be9d3cc54148d760128d0bcdd33f
hezhiqiang <123456789>
2017-06-13 22:29:04 +0800

    接口

--- dlfc-zfw-wechat-house/src/main/java/com/dlfc/services/house/convertor/SysInfoAttConvertor.java
+++ dlfc-zfw-wechat-house/src/main/java/com/dlfc/services/house/convertor/SysInfoAttConvertor.java
@@ -1,15 +1,24 @@
 package com.dlfc.services.house.convertor;
 
 import com.dlfc.services.house.entity.SysInfoAtt;
+import com.dlfc.services.house.enums.InfoAttFileTypeEnum;
 import com.housecenter.dlfc.commons.bases.convertor.AbstractConvertor;
+import com.housecenter.dlfc.framework.common.util.StringUtils;
 
 /**
  * Created by K on 2017/6/13.
  */
 public class SysInfoAttConvertor extends AbstractConvertor<SysInfoAtt, String> {
     @Override
-    public SysInfoAtt toModel(String s) {
-        return null;
+    public SysInfoAtt toModel(String str) {
+        SysInfoAtt model = new SysInfoAtt();
+        if (StringUtils.isNotEmpty(str)) {
+            int i = str.indexOf("/");
+            model.setFileName(str.substring(0, i));
+            model.setFilePath(str.substring(i));
+            model.setFileType(InfoAttFileTypeEnum.HOUSE_PIC_ENUM.getValue());
+        }
+        return model;
     }
 
     @Override
