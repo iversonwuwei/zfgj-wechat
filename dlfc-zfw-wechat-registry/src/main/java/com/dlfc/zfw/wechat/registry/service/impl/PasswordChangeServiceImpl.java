4fa75bc511ebf3e92ef0bd968032baa4091a0834
wuwei <iverson.wuwei@gmail.com>
2017-06-14 17:24:42 +0800

    update the code

--- dlfc-zfw-wechat-registry/src/main/java/com/dlfc/zfw/wechat/registry/service/impl/PasswordChangeServiceImpl.java
+++ dlfc-zfw-wechat-registry/src/main/java/com/dlfc/zfw/wechat/registry/service/impl/PasswordChangeServiceImpl.java
@@ -5,6 +5,7 @@ import com.dlfc.zfw.wechat.registry.dto.UserVDTO;
 import com.dlfc.zfw.wechat.registry.entity.UsrUser;
 import com.dlfc.zfw.wechat.registry.repository.RegistryRService;
 import com.dlfc.zfw.wechat.registry.repository.UpdateRService;
+import com.dlfc.zfw.wechat.registry.service.EncryptService;
 import com.dlfc.zfw.wechat.registry.service.PasswordChangeService;
 import com.dlfc.zfw.wechat.registry.service.UsrUserService;
 import com.housecenter.dlfc.commons.bases.convertor.base.IConvertor;
@@ -18,11 +19,14 @@ public class PasswordChangeServiceImpl implements PasswordChangeService<UserVDTO
     private UsrUserService<UserVDTO> usrUserService;
     @Autowired
     private UpdateRService updateRService;
+    @Autowired
+    private EncryptService encryptService;
 
     @Override
     public String change(UserVDTO o) {
         UsrUser usrUser = usrUserService.findUser(o);
         if (usrUser != null){
+            usrUser.setPassword(encryptService.generatePassword(o.getPassword()));
             return updateRService.updateById(usrUser);
         }
         return null;
