c3bee898bf51662f24f5ddc6cc6e9c6a5bdbe50e
wuwei <iverson.wuwei@gmail.com>
2017-06-21 11:32:00 +0800

    update the code

---
+++ dlfc-zfw-wechat-validate/src/main/java/com/dlfc/services/validate/entity/User.java
@@ -0,0 +1,25 @@
+package com.dlfc.services.validate.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+
+    private String perId;
+
+    private String id;
+
+    private String mobile;
+
+    private String degree;
+
+    private String version;
+
+    private String username;
+
+    private String email;
+
+    private String agtCert;
+}
