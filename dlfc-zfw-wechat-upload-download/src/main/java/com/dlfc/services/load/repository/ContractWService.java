9ace2c02fb7aadad652b6b21d14ac6a6a628437a
hezhiqiang <hezhiqiang123>
2017-06-27 07:08:20 +0800

    合同相关

---
+++ dlfc-zfw-wechat-upload-download/src/main/java/com/dlfc/services/load/repository/ContractWService.java
@@ -0,0 +1,21 @@
+package com.dlfc.services.load.repository;
+
+import com.housecenter.dlfc.commons.bases.dto.ResultDTO;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by K on 6/26/17.
+ */
+
+@FeignClient("dlfc-zfw-contract-services")
+public interface ContractWService {
+
+    @RequestMapping(value = "/ww/contractSign/lessorSign", method = RequestMethod.GET)
+    ResultDTO<Void> lessorSign(@RequestParam String id, @RequestParam String path);
+
+    @RequestMapping(value = "/ww/contractSign/lesseeSign", method = RequestMethod.GET)
+    ResultDTO<Void> lesseeSign(@RequestParam String id, @RequestParam String path);
+}
