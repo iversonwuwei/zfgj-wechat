9160e2e6beed3153ba0c31a009af33e906ba713f
wuwei <iverson.wuwei@gmail.com>
2017-07-05 19:25:54 +0800

    update the code

--- dlfc-zfw-wechat-house/src/main/java/com/dlfc/services/house/service/impl/HouseLeaseInfoServiceImpl.java
+++ dlfc-zfw-wechat-house/src/main/java/com/dlfc/services/house/service/impl/HouseLeaseInfoServiceImpl.java
@@ -63,7 +63,7 @@ public class HouseLeaseInfoServiceImpl implements HouseLeaseInfoService {
             result = lesseeRService.findByParams(dto);
             entityList = convertor.convert2Objects(result, HouLeaseInfo.class);
         }
-        List<String> facilityIdList = dto.getFacilityIdList();
+        /*List<String> facilityIdList = dto.getFacilityIdList();
         if (null == facilityIdList || facilityIdList.size() == 0) {
             return entityList;
         }
@@ -88,8 +88,9 @@ public class HouseLeaseInfoServiceImpl implements HouseLeaseInfoService {
             if (flag) {
                 houLeaseInfoList.add(item);
             }
-        }
-        return houLeaseInfoList;
+        }*/
+        return entityList;
+        //return houLeaseInfoList;
     }
 
     @Override
