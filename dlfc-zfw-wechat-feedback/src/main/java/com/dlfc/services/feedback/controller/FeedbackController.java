f7c95fefe6d3557198169ae3162d7b793c09b101
wuwei <iverson.wuwei@gmail.com>
2017-07-06 16:12:03 +0800

    update the code

--- dlfc-zfw-wechat-feedback/src/main/java/com/dlfc/services/feedback/controller/FeedbackController.java
+++ dlfc-zfw-wechat-feedback/src/main/java/com/dlfc/services/feedback/controller/FeedbackController.java
@@ -3,22 +3,18 @@ package com.dlfc.services.feedback.controller;
 
 import com.dlfc.admin.common.utils.StringUtils;
 import com.dlfc.services.feedback.convertor.CmsGuestBookConvertor;
-import com.dlfc.services.feedback.convertor.FeedbackConvertor;
 import com.dlfc.services.feedback.dto.FeedbackDTO;
-import com.dlfc.services.feedback.dto.UserDTO;
 import com.dlfc.services.feedback.repository.UserInfoRService;
 import com.dlfc.services.feedback.service.CmsGuestbookService;
 import com.dlfc.services.feedback.service.UsrPersonService;
 import com.dlfc.zfw.wechat.entities.entity.CmsGuestbook;
 import com.dlfc.zfw.wechat.entities.entity.SysPerson;
 import com.dlfc.zfw.wechat.entities.entity.UsrUser;
-import com.housecenter.dlfc.commons.bases.convertor.base.IConvertor;
 import com.housecenter.dlfc.commons.bases.dto.ResultDTO;
 import com.housecenter.dlfc.commons.bases.error.ResultError;
 import com.housecenter.dlfc.framework.ca.api.PrincipalService;
 import com.housecenter.dlfc.framework.common.web.AjaxResult;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -39,23 +35,32 @@ public class FeedbackController {
 
     @RequestMapping(method = RequestMethod.POST)
     public ResultDTO feedback(@RequestBody FeedbackDTO feedbackDTO, @RequestHeader String token, HttpServletRequest httpRequest) {
-        AjaxResult user = principalService.principal(token);
-        UsrUser usrUser = userInfoRService.findUserByUser(user.getData().toString().substring(0, 11));
+        AjaxResult user;
+        UsrUser usrUser;
         SysPerson sysPerson;
-        if (usrUser != null) {
-            try {
-                sysPerson = usrPersonService.findById(usrUser.getPerId());
+        try {
+            user = principalService.principal(token);
+            usrUser = userInfoRService.findUserByUser(user.getData().toString().substring(0, 11));
+            if (usrUser != null) {
+                //sysPerson = usrPersonService.findById(usrUser.getPerId());
                 feedbackDTO.setUid(usrUser.getId());
-                if (sysPerson != null) {
+                feedbackDTO.setName(usrUser.getUsername());
+                /*if (sysPerson != null) {
                     feedbackDTO.setName(sysPerson.getName());
-                }
+                }*/
                 feedbackDTO.setIp(StringUtils.getRemoteAddr(httpRequest));
                 feedbackDTO.setPhoneNumber(usrUser.getMobile());
-            }catch (Exception e){
+            }
+        }catch (Exception e){
+            if (e.getMessage().contains("认证失效")){
                 ResultError resultError = new ResultError();
-                resultError.setErrcode("190");
-                resultError.setErrmsg("请先实名认证");
-
+                resultError.setErrmsg("认证失效");
+                resultError.setErrcode("250");
+                return ResultDTO.failure(resultError);
+            }else {
+                ResultError resultError = new ResultError();
+                resultError.setErrmsg("请重新登录！");
+                resultError.setErrcode("280");
                 return ResultDTO.failure(resultError);
             }
         }
