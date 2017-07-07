e6128034559cffead522a58d1313537cacbad1d8
wuwei <iverson.wuwei@gmail.com>
2017-07-07 14:21:01 +0800

    update the code

--- dlfc-zfw-wechat-validate/src/main/java/com/dlfc/services/validate/controller/UserValidateController.java
+++ dlfc-zfw-wechat-validate/src/main/java/com/dlfc/services/validate/controller/UserValidateController.java
@@ -5,6 +5,7 @@ import com.dlfc.services.validate.dto.ValidateResultDTO;
 import com.dlfc.services.validate.repository.UserInfoRService;
 import com.dlfc.zfw.wechat.entities.entity.UsrUser;
 import com.housecenter.dlfc.commons.bases.dto.ResultDTO;
+import com.housecenter.dlfc.commons.bases.error.ResultError;
 import com.housecenter.dlfc.framework.ca.api.PrincipalService;
 import com.housecenter.dlfc.framework.common.web.AjaxResult;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,13 +29,27 @@ public class UserValidateController {
     private UserValidateConvertor userValidateConvertor;
 
     @RequestMapping(value = "/user", method = RequestMethod.GET)
-    public ResultDTO<ValidateResultDTO> userValidation(@RequestParam String token) {
+    public ResultDTO userValidation(@RequestParam String token) {
         AjaxResult user = null;
         UsrUser userEntity = null;
-        if (!token.equals("") && token != null) {
-            user = principalService.principal(token);
-            //userEntity = convertor.convert2Object(userInfoRService.findUserByUser(user.getData().toString()), User.class);
-            userEntity = userInfoRService.findUserByUser(user.getData().toString());
+        try {
+            if (!token.equals("") && token != null) {
+                user = principalService.principal(token);
+                //userEntity = convertor.convert2Object(userInfoRService.findUserByUser(user.getData().toString()), User.class);
+                userEntity = userInfoRService.findUserByUser(user.getData().toString());
+            }
+        }catch (Exception e){
+            ResultError resultError;
+            if (e.getMessage().contains("认证失效")) {
+                resultError = new ResultError();
+                resultError.setErrcode("250");
+                resultError.setErrmsg("认证失效！");
+            }else {
+                resultError = new ResultError();
+                resultError.setErrcode("280");
+                resultError.setErrmsg("请重新登录！");
+            }
+            return ResultDTO.failure(resultError);
         }
         return userValidateConvertor.toResultDTO(userEntity);
     }
