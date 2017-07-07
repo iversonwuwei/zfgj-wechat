17a84aab965a19e111a32be5526096d1bdbcb222
hezhiqiang <hezhiqiang123>
2017-07-07 17:08:12 +0800

    用户验证相关

--- dlfc-zfw-wechat-house/src/main/java/com/dlfc/services/house/service/impl/UsrUserServiceImpl.java
+++ dlfc-zfw-wechat-house/src/main/java/com/dlfc/services/house/service/impl/UsrUserServiceImpl.java
@@ -3,7 +3,6 @@ package com.dlfc.services.house.service.impl;
 import com.dlfc.services.house.repository.UserInfoRService;
 import com.dlfc.services.house.service.UsrUserService;
 import com.dlfc.zfw.wechat.entities.entity.UsrUser;
-import com.housecenter.dlfc.commons.bases.convertor.base.IConvertor;
 import com.housecenter.dlfc.framework.boot.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -14,19 +13,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class UsrUserServiceImpl implements UsrUserService {
     @Autowired
     private UserInfoRService userInfoRService;
-    @Autowired
-    private IConvertor<UsrUser> convertor;
 
     @Override
     public UsrUser findById(String uid) {
-        String user = null;
-        if (uid != null) {
-            user = userInfoRService.findUserById(uid);
-        }
-        UsrUser usrUser = null;
-        if (user != null) {
-            usrUser = convertor.convert2Object(user, UsrUser.class);
-        }
-        return usrUser;
+        return userInfoRService.findUserById(uid);
     }
 }
